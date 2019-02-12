9585539d079a80ea0836e6e6b5d1c66c80aefd45
Zuldajri <w.matyas15@hotmail.fr>
2019-02-12 13:01:22 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,1 +1,6 @@
+#Variables
+$URI = "http://download.veeam.com/VeeamBackup&Replication_9.5.4.2615.Update4.iso"
+
+#Get VCC iso
+Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile VeeamBackupReplication.iso
 
