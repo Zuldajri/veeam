6a186da73a51bc740ff5c4c91e5a3174b5369944
Zuldajri <w.matyas15@hotmail.fr>
2019-02-22 16:46:17 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -4,18 +4,11 @@ $URI = "http://download.veeam.com/VeeamBackup&Replication_9.5.4.2615.Update4.iso
 #Get VCC iso
 Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile VeeamBackupReplication.iso
 
-$disks = Get-Disk | sort number
-$letters = 70..89 | ForEach-Object { [char]$_ }
-$count = 0
-$labels = "data1","data2"
+Get-Disk | ` 
+Where partitionstyle -eq 'raw' | ` 
+Initialize-Disk -PartitionStyle MBR -PassThru | ` 
+New-Partition -AssignDriveLetter -UseMaximumSize | ` 
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
 
-foreach ($disk in $disks) {
-       $driveLetter = $letters[$count].ToString()
-       $disk | 
-       New-Partition -UseMaximumSize -DriveLetter $driveLetter |
-       Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
-    $count++
-    }
-
-    $iso = Get-ChildItem -Path "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.3\Downloads\0\VeeamBackupReplication.iso"
+$iso = Get-ChildItem -Path "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.3\Downloads\0\VeeamBackupReplication.iso"
 
