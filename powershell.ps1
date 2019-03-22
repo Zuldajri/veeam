18911c07fd9742a386a4a4b96d74471adee30862
Zuldajri <w.matyas15@hotmail.fr>
2019-03-22 13:41:03 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,9 +1,12 @@
 #Variables
-$URI = "http://download.veeam.com/VeeamBackup&Replication_9.5.4.2615.Update4.iso"
+$url = "http://download.veeam.com/VeeamBackup&Replication_9.5.4.2615.Update4.iso"
+$output = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.3\Downloads\0\VeeamBackupReplication.iso"
 
 #Get VCC iso
-Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile VeeamBackupReplication.iso
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
+#Initialize Data Disks
 Get-Disk | ` 
 Where partitionstyle -eq 'raw' | ` 
 Initialize-Disk -PartitionStyle MBR -PassThru | ` 
