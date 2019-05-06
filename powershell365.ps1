5384d8d2b78a946a46cfc7740ae5c642bb7f8816
Zuldajri <w.matyas15@hotmail.fr>
2019-05-06 20:12:13 +0200

    Update powershell365.ps1

--- powershell365.ps1
+++ powershell365.ps1
@@ -1,21 +1,13 @@
 [CmdletBinding()]
 
 Param(
-  [string]$USERNAME,
-  [string]$GuestOSName,
-  [string]$PASSWORD
+  [string] $USERNAME,
+  [string] $GuestOSName,  
+  [string] $PASSWORD
  )
 
-#Configure logging
-function log
-{
-   param([string]$message)
-   "`n`n$(get-date -f o)  $message" 
-}
-
-try
-{
 # Modify the $url 
+
 #Variables
 $url = "http://download.veeam.com/VeeamBackupOffice365_3.0.0.422.zip"
 $output = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.9.3\Downloads\0\VeeamBackupOffice365_3.0.0.422.zip"
@@ -67,69 +59,24 @@ $MSIArguments = @(
 )
 Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
 
+#Create a credential
+#log "Creating credentials"
 $fulluser = "$($GuestOSName)\$($USERNAME)"
+$secpasswd = ConvertTo-SecureString $PASSWORD -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential($fulluser, $secpasswd)
+
+#Impersonate user
+#log "Impersonate user '$AdminUser'"
+.\New-ImpersonateUser.ps1 -Credential $mycreds
 
 
-#Enable CredSSP	
-Enable-WSManCredSSP -Role Server –Force
-Enable-WSManCredSSP -Role Client -DelegateComputer $GuestOSName -Force
-Enable-PSRemoting –force
-Set-Item WSMan:\localhost\Client\TrustedHosts * -Force
-
-#Set policy "Allow delegating fresh credentials with NTLM-only server authentication" 
-$allowed = @('WSMAN/'+ $GuestOSName)
-$key = 'hklm:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation'
-if (!(Test-Path $key)) {
-	md $key
-}  
-New-ItemProperty -Path $key -Name AllowFreshCredentialsWhenNTLMOnly  -Value 1 -PropertyType Dword -Force    
-$key = Join-Path $key 'AllowFreshCredentialsWhenNTLMOnly'
-if (!(Test-Path $key)) {
-	md $key
-}
-$i = 1
-	$allowed |% {
-		# Script does not take into account existing entries in this key
-		New-ItemProperty -Path $key -Name $i -Value $_ -PropertyType String -Force
-		$i++
-	}
-
-    #Create a credential
-    log "Creating credentials"
-    $secpasswd = ConvertTo-SecureString $PASSWORD -AsPlainText -Force
-    $mycreds = New-Object System.Management.Automation.PSCredential ($fulluser, $secpasswd)
-
-    #Impersonate user
-    log "Impersonate user '$fulluser'"
-    .\New-ImpersonateUser.ps1 -Credential $mycreds
-
-
+Connect-VBOServer -Credential $mycreds
 $Driveletter = get-wmiobject -class "Win32_Volume" -namespace "root\cimv2" | where-object {$_.DriveLetter -like "F*"}
 $VeeamDrive = $DriveLetter.DriveLetter
 $repo = "$($VeeamDrive)\backup repository"
 New-Item -ItemType Directory -path $repo -ErrorAction SilentlyContinue
+$proxy = Get-VBOProxy
 
-  
-$scriptblock= {
-Connect-VBOServer
-$proxy = Get-VBOProxy 
 Add-VBORepository -Proxy $proxy -Name "Default Backup Repository 1" -Path "F:\backup repository" -Description "Default Backup Repository 1" -RetentionType ItemLevel
-}
+  
 
-$session = New-PSSession -cn $env:computername -Credential $mycreds -Authentication Credssp
-	Invoke-Command -Session $session -ScriptBlock $scriptblock
-	Remove-PSSession -VMName $env:computername
-
-}
-catch
-{
-    log "ERROR: Exception caught - '$_.Exception.Message' - '$_.Exception.ItemName'"
-    throw
-}
-finally
-{
-    log "All Done"
-}
-
-
- 
