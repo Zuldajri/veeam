d5c7f377c2385aa8b364309a123891e766c9e99d
Zuldajri <w.matyas15@hotmail.fr>
2020-02-13 12:02:20 +0100

    Update v10powershell365.ps1

--- v10powershell365.ps1
+++ v10powershell365.ps1
@@ -9,8 +9,8 @@ Param(
 # Modify the $url 
 
 #Variables
-$url = "http://download.veeam.com/VeeamBackupOffice365_4.0.0.1345.zip"
-$output = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\VeeamBackupOffice365_4.0.0.1345.zip"
+$url = "http://download.veeam.com/VeeamBackupOffice365_4.0.0.2516.zip"
+$output = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\VeeamBackupOffice365_4.0.0.2516.zip"
 
 #Get Veeam Backup for Office 365 zip
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
@@ -22,14 +22,14 @@ Initialize-Disk -PartitionStyle GPT -PassThru | `
 New-Partition -AssignDriveLetter -UseMaximumSize | ` 
 Format-Volume -FileSystem ReFS -NewFileSystemLabel "datadisk" -Confirm:$false
 
-Expand-Archive C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\VeeamBackupOffice365_4.0.0.1345.zip -DestinationPath C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\ -Force
+Expand-Archive C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\VeeamBackupOffice365_4.0.0.2516.zip -DestinationPath C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\ -Force
 
 $source = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension"
 
 ### Veeam Backup Office 365
 $MSIArguments = @(
 "/i"
-"$source\Veeam.Backup365_4.0.0.1345.msi"
+"$source\Veeam.Backup365_4.0.0.2516.msi"
 "/qn"
 "ADDLOCAL=BR_OFFICE365,CONSOLE_OFFICE365,PS_OFFICE365"
 "ACCEPT_THIRDPARTY_LICENSES=1"
@@ -40,7 +40,7 @@ Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
 ### Veeam Explorer for Microsoft Exchange
 $MSIArguments = @(
 "/i"
-"$source\VeeamExplorerForExchange_4.0.0.1345.msi"
+"$source\VeeamExplorerForExchange_10.0.0.443.msi"
 "/qn"
 "ADDLOCAL=BR_EXCHANGEEXPLORER,PS_EXCHANGEEXPLORER"
 "ACCEPT_THIRDPARTY_LICENSES=1"
@@ -51,7 +51,7 @@ Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
 ### Veeam Explorer for Microsoft SharePoint
 $MSIArguments = @(
 "/i"
-"$source\VeeamExplorerForSharePoint_4.0.0.1345.msi"
+"$source\VeeamExplorerForSharePoint_10.0.0.443.msi"
 "/qn"
 "ADDLOCAL=BR_SHAREPOINTEXPLORER,PS_SHAREPOINTEXPLORER"
 "ACCEPT_THIRDPARTY_LICENSES=1"
